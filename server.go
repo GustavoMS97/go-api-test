83d3caec276508438a3012023db9689f365bfa11
GustavoMS97 <gustavo.santos@pgmais.com.br>
2019-09-09 11:17:16 -0300

    Created a connection to mongoDB and used the first API's to create data inside the database.

--- server.go
+++ server.go
@@ -1,13 +1,22 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
 	"math/rand"
 	"net/http"
 	"strings"
 
 	"github.com/labstack/echo"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
+
+var clientMongoDb mongo.Client
+var userCollection *mongo.Collection
 
 type User struct {
 	Name  string `json:"name,omitempty"`
@@ -22,7 +31,13 @@ func saveUser(c echo.Context) error {
 	}
 	user.Name = fmt.Sprintf("%v", m["name"])
 	user.Email = fmt.Sprintf("%v", m["email"])
-	return c.JSON(200, user)
+
+	insertResult, err := userCollection.InsertOne(context.TODO(), user)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return c.JSON(200, insertResult.InsertedID)
 }
 
 func desafio1(c echo.Context) error {
@@ -53,22 +68,70 @@ func desafio2(c echo.Context) error {
 }
 
 func getUser(c echo.Context) error {
+	var result User
 	id := c.Param("id")
-	return c.String(http.StatusOK, id)
+	objID, _ := primitive.ObjectIDFromHex(id)
+	filter := bson.D{{"_id", objID}}
+	err := userCollection.FindOne(context.TODO(), filter).Decode(&result)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return c.JSON(http.StatusOK, result)
 }
 
 func show(c echo.Context) error {
-	team := c.QueryParam("team")
-	member := c.QueryParam("member")
-	return c.String(http.StatusOK, "team:"+team+", member:"+member)
+	var results []*User
+	cur, err := userCollection.Find(context.TODO(), bson.D{})
+	if err != nil {
+		log.Fatal(err)
+	}
+	for cur.Next(context.TODO()) {
+		var elem User
+		err := cur.Decode(&elem)
+		if err != nil {
+			log.Fatal(err)
+		}
+		results = append(results, &elem)
+	}
+	if err := cur.Err(); err != nil {
+		log.Fatal(err)
+	}
+	cur.Close(context.TODO())
+	return c.JSON(http.StatusOK, results)
+}
+
+func connectToMongodb() {
+	// Set client options
+	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
+	// Connect to MongoDB
+	clientMongoDb, err := mongo.Connect(context.TODO(), clientOptions)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// Check the connection
+	err = clientMongoDb.Ping(context.TODO(), nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Connected to MongoDB!")
+	userCollection = clientMongoDb.Database("GO_TEST_API").Collection("users")
+}
+
+func disconnectFromMongodb() {
+	err := clientMongoDb.Disconnect(context.TODO())
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Connection to MongoDB closed.")
 }
 
 func main() {
+	connectToMongodb()
 	e := echo.New()
 	e.POST("/users", saveUser)
 	e.GET("/users/:id", getUser)
-	e.GET("/show", show)
+	e.GET("/users", show)
 	e.GET("/desafio1", desafio1)
 	e.GET("/desafio2", desafio2)
-	e.Logger.Fatal(e.Start(":3333"))
+	e.Logger.Fatal(e.Start(":3000"))
 }
