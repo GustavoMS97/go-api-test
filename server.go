3073655b6b4d79b8be64d5c1d9342012b9dccabd
GustavoMS97 <gustavo.santos@pgmais.com.br>
2019-09-09 13:07:20 -0300

    Small corrections and system variables beeing used.

--- server.go
+++ server.go
@@ -6,9 +6,11 @@ import (
 	"log"
 	"math/rand"
 	"net/http"
+	"os"
 	"strings"
 
 	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
@@ -39,33 +41,6 @@ func saveUser(c echo.Context) error {
 	}
 
 	return c.JSON(200, insertResult.InsertedID)
-}
-
-func desafio1(c echo.Context) error {
-	var result []string
-	var naipes = []string{"C", "E", "P", "O"}
-	var valores = []string{"A", "2", "3", "4", "5", "6", "7", "8", "9", "T", "J", "Q", "K"}
-	for i := 0; i < len(naipes); i++ {
-		for j := 0; j < len(valores); j++ {
-			result = append(result, naipes[i]+valores[j]+",")
-		}
-	}
-	return c.String(http.StatusOK, "["+strings.Join(result, " ")+"]")
-}
-
-func desafio2(c echo.Context) error {
-	var result []string
-	var naipes = []string{"C", "E", "P", "O"}
-	var valores = []string{"A", "2", "3", "4", "5", "6", "7", "8", "9", "T", "J", "Q", "K"}
-	for i := 0; i < len(naipes); i++ {
-		for j := 0; j < len(valores); j++ {
-			result = append(result, naipes[i]+valores[j])
-		}
-	}
-	var carta1 = rand.Intn(len(result)-0) + 0
-	var carta2 = rand.Intn(len(result)-0) + 0
-	response := fmt.Sprintf("Carta 1: %s\nCarta 2: %s", result[carta1], result[carta2])
-	return c.String(http.StatusOK, response)
 }
 
 func getUser(c echo.Context) error {
@@ -102,8 +77,14 @@ func show(c echo.Context) error {
 }
 
 func connectToMongodb() {
+	mongoURL := os.Getenv("MONGO_URL")
+	if mongoURL == "" {
+		fmt.Println("url mongo vazia.")
+		mongoURL = "mongodb://localhost:27017"
+	}
+	fmt.Println(mongoURL)
 	// Set client options
-	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
+	clientOptions := options.Client().ApplyURI(mongoURL)
 	// Connect to MongoDB
 	clientMongoDb, err := mongo.Connect(context.TODO(), clientOptions)
 	if err != nil {
@@ -126,13 +107,51 @@ func disconnectFromMongodb() {
 	fmt.Println("Connection to MongoDB closed.")
 }
 
+func desafio1(c echo.Context) error {
+	var result []string
+	var naipes = []string{"C", "E", "P", "O"}
+	var valores = []string{"A", "2", "3", "4", "5", "6", "7", "8", "9", "T", "J", "Q", "K"}
+	for i := 0; i < len(naipes); i++ {
+		for j := 0; j < len(valores); j++ {
+			result = append(result, naipes[i]+valores[j]+",")
+		}
+	}
+	return c.String(http.StatusOK, "["+strings.Join(result, " ")+"]")
+}
+
+func desafio2(c echo.Context) error {
+	var result []string
+	var naipes = []string{"C", "E", "P", "O"}
+	var valores = []string{"A", "2", "3", "4", "5", "6", "7", "8", "9", "T", "J", "Q", "K"}
+	for i := 0; i < len(naipes); i++ {
+		for j := 0; j < len(valores); j++ {
+			result = append(result, naipes[i]+valores[j])
+		}
+	}
+	var carta1 = rand.Intn(len(result)-0) + 0
+	var carta2 = rand.Intn(len(result)-0) + 0
+	response := fmt.Sprintf("Carta 1: %s\nCarta 2: %s", result[carta1], result[carta2])
+	return c.String(http.StatusOK, response)
+}
+
 func main() {
 	connectToMongodb()
 	e := echo.New()
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"*"},
+		AllowHeaders: []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept},
+	}))
 	e.POST("/users", saveUser)
 	e.GET("/users/:id", getUser)
 	e.GET("/users", show)
 	e.GET("/desafio1", desafio1)
 	e.GET("/desafio2", desafio2)
-	e.Logger.Fatal(e.Start(":3000"))
+	port := os.Getenv("PORT")
+	if port == "" {
+		fmt.Println("Porta vazia.")
+		port = "3000"
+	}
+	fmt.Println(port)
+	e.Logger.Fatal(e.Start(":" + port))
+	//MONGO_URL=mongodb+srv://GUSTAVOMS97:Gu4525214@cluster0-gvhbx.mongodb.net go run server.go
 }
